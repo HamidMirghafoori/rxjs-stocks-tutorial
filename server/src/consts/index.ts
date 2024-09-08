33917d24c3ef9eb3ff6b391ef2b78125853eaa30
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-09-08 22:03:45 +0100

    [server] updating stocks-price route

--- server/src/consts/index.ts
+++ server/src/consts/index.ts
@@ -1,9 +1,9 @@
-import { StocksType } from "../models";
+import { StocksType } from '../models';
 
 // Mock data for stock prices
 export const stocks: StocksType[] = [
-  { symbol: 'AAPL', price: 150.25 },
-  { symbol: 'GOOGL', price: 2800.10 },
-  { symbol: 'MSFT', price: 305.00 },
-  { symbol: 'AMZN', price: 3400.50 }
+  { symbol: 'AAPL', price: 150.25, lastPrice: 148.2 },
+  { symbol: 'GOOGL', price: 2800.1, lastPrice: 1810.35 },
+  { symbol: 'MSFT', price: 305.0, lastPrice: 303.2 },
+  { symbol: 'AMZN', price: 3400.5, lastPrice: 3600.7 },
 ];
