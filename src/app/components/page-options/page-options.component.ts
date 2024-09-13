adae9db1fb2aa583a36bb35a05f0ff03d860ebda
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-09-13 18:57:26 +0100

    NIC enhancing PageOptionsComponent

--- src/app/components/page-options/page-options.component.ts
+++ src/app/components/page-options/page-options.component.ts
@@ -11,4 +11,5 @@ import { Component, Input } from '@angular/core';
 export class PageOptionsComponent {
 @Input() options: string[]=[];
 @Input() action!: (option: string) => void;
+@Input() direction: string = 'row';
 }
