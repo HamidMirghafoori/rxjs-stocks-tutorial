ba574238dbccdb65d6c766cc928dbb41fa42d2d3
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-09-13 20:36:38 +0100

    [server] ts-node and CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,20 @@
+import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
 import { stocks } from './consts';
 
 const app: Express = express();
 const port: number = 3000;
 
-app.get('/stocks-price', (req, res) => {
-  res.json(stocks);
-});
+app.use(cors());
 
 app.get('/stocks-price-delayed', (req, res) => {
   setTimeout(() => {
     res.json(stocks);
-  }, 1000);
+  }, 3000);
+});
+
+app.get('/stocks-price', (req, res) => {
+  res.json(stocks);
 });
 
 app.get('/', (req: Request, res: Response) => {
