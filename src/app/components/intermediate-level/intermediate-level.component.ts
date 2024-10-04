2efc7e78ff1f6b9b7011067078c258aaa2e88125
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-04 21:52:22 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Observable } from 'rxjs';
+import {
+  catchError,
+  interval,
+  map,
+  Observable,
+  of
+} from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -17,5 +23,31 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.retry();
+  }
+
+  /**
+   * we are continuing from catchError2 branch with minor changes
+   * Currently we have an infinite loop of errors to avoid that
+   * add retry(3) to repeat the subscription for three times
+   * This time we want to use Javascript error throw function, so we replaced throwError with throw
+   * note that because we are returning a value instead of observable (throwError is an observable) so we have to return value, not of(value)
+   */
+  private retry() {
+    interval(1000)
+      .pipe(
+        map((value) => {
+          if (value === 4) {
+            console.log('Error happened....');
+            throw 'We hit error 3 time!';
+          }
+          return value;
+        }),
+        catchError((err) => {
+          console.log('Error caught: ', err);
+          return of(-1);
+        })
+      )
+      .subscribe((value) => console.log(JSON.stringify(value)));
   }
 }
