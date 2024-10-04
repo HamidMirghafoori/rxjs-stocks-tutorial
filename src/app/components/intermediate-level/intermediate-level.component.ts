c28615226fd500852663403babcfdedba85b1391
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-04 21:44:36 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Observable } from 'rxjs';
+import {
+  catchError,
+  interval,
+  mergeMap,
+  of,
+  throwError
+} from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -12,10 +18,33 @@ import { TerminalComponent } from '../terminal/terminal.component';
   styleUrl: './intermediate-level.component.css',
 })
 export class IntermediateLevelComponent implements OnInit {
-  public errorObservable$!: Observable<string>;
   public constructor(private log: LogService) {}
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.catchError2();
+  }
+
+  /**
+   * we are continuing from throwError branch with minor changes
+   * now we want to continue the interval from 0 again after the error occurs by returning caught observable
+   * instead of the last value of -1, 
+   * update the code to utilize returning caught and getting rid of returned -1 value
+   */
+  private catchError2() {
+    interval(1000)
+      .pipe(
+        mergeMap((value) => {
+          if (value === 4) {
+            return throwError(() => 'We hit number 4!');
+          }
+          return of(value);
+        }),
+        catchError((err) => {
+          console.log('Error caught: ', err);
+          return of(-1);
+        })
+      )
+      .subscribe((value) => console.log(value));
   }
 }
