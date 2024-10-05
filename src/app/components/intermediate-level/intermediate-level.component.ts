bf244c43e90d7ed2381ab3633867a536988803d5
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-05 20:34:17 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { catchError, delay, of, timeout } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -18,11 +19,18 @@ export class IntermediateLevelComponent implements OnInit {
     this.timeout();
   }
 
-/**
- * This is simple task, we want to create an observable that emits 'Hello' and then waits for a long time
- * using timeout() we throw an error and message the timeout error so this way we could complete the observable 
- */
+  /**
+   * This is simple task, we want to create an observable that emits 'Hello' and then waits for a long time
+   * using timeout(3000) we throw an error and message the timeout error so this way we could complete the observable
+   * Note that because we applied the logic for the first emission thus we won't see Hello logged. to fix that we need to use different approach.
+   */
   private timeout() {
-
+    of('Hello')
+      .pipe(
+        delay(200000),
+        timeout(3000),
+        catchError((error) => of('Error: ', error))
+      )
+      .subscribe((res) => console.log(res));
   }
 }
