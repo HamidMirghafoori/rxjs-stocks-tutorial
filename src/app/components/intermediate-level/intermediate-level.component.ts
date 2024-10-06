c29b54a610d5130c7e6883ab9be134281c876162
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-06 12:21:27 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,19 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.subject();
+  }
+
+  private subject() {
+    /**
+     * Question 1: Multi-casting
+     * We want to create a subject named multiCaster$ and three subscribers with full signature which
+     * each has identifiable log to relate log to the subscription.
+     * Every time we emit new value we like to see subscribers logged the value
+     * Steps to take: create 1st subscriber then emit value 5
+     * create 2nd subscriber and emit value 22
+     * create 3rd subscriber and emit value 7
+     * complete the subject and emit value 100, and observe what happens
+     */
   }
 }
