10e16f233d50a29b3818a9d29c7426d7c1604a00
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-06 21:58:41 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,22 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.behaviorSubject();
+  }
+
+  public behaviorSubject() {
+    /**
+     * We want to create a behaviorSubject named multiCaster$ with default value 3
+     * then three subscribers with full signature which
+     * each has identifiable log to relate log to the subscription.
+     * Every time we emit new value we like to see subscribers logged the value
+     * Steps to take: 
+     * before each subscription have a console.log such as 'Before subscription 1' and so on.
+     * create 1st subscriber then emit value 4
+     * create 2nd subscriber and emit value 17
+     * create 3rd subscriber and emit value 9
+     * complete the multiCaster$ and emit value 100, and observe what happens
+     * We can see each subscription receives the latest emitted value before its subscription
+     */
   }
 }
