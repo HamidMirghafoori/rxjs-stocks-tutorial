df4a4d755f50d450b5b8aa138e6f96eb97d5c4cd
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-06 12:25:38 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Subject } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -12,6 +13,7 @@ import { TerminalComponent } from '../terminal/terminal.component';
 })
 export class IntermediateLevelComponent implements OnInit {
   public constructor(private log: LogService) {}
+  private multiCaster$: Subject<number> = new Subject<number>();
 
   public ngOnInit(): void {
     this.log.clearLogs();
@@ -28,6 +30,30 @@ export class IntermediateLevelComponent implements OnInit {
      * create 2nd subscriber and emit value 22
      * create 3rd subscriber and emit value 7
      * complete the subject and emit value 100, and observe what happens
+     * Note that: After the subject is completed, calling next() (or error()) will not have any effect. 
+     * The observable is terminated, and further emissions are ignored. 
+     * This is part of the RxJS specification: once an observable completes, 
+     * it is in a "final state" and cannot emit more values or errors.
      */
+    this.multiCaster$.subscribe({
+      next: (value) => console.log('Subscriber 1 received value ->', value),
+      error: (error) => console.log('Error happened for 1st subscriber: ', error),
+      complete: () => console.log('Task 1 is completed!'),
+    });
+    this.multiCaster$.next(5);
+    this.multiCaster$.subscribe({
+      next: (value) => console.log('Subscriber 2 received value ->', value),
+      error: (error) => console.log('Error happened for 2nd subscriber: ', error),
+      complete: () => console.log('Task 2 is completed!'),
+    });
+    this.multiCaster$.next(22);
+    this.multiCaster$.subscribe({
+      next: (value) => console.log('Subscriber 3 received value ->', value),
+      error: (error) => console.log('Error happened for 3rd subscriber: ', error),
+      complete: () => console.log('Task 3 is completed!'),
+    });
+    this.multiCaster$.next(7);
+    this.multiCaster$.complete();
+    this.multiCaster$.next(100);
   }
 }
