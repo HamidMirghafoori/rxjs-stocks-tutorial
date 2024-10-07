7d19d08e65e380fae80c2ab40e1cd6583f34aca6
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-07 22:14:24 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { combineLatest, delay, finalize, interval, map, take } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -30,6 +31,39 @@ export class IntermediateLevelComponent implements OnInit {
      *   and proper message for each callback
      * Note how the values are emitted from combineLatest()
      */
- 
+     const source1$ = interval(100).pipe(
+      take(20),
+      finalize(() => console.log('source1$ has completed!'))
+    );
+    const source2$ = interval(500).pipe(
+      delay(1000),
+      take(3),
+      finalize(() => console.log('source2$ has completed!'))
+    );
+    const source3$ = interval(400).pipe(
+      take(10),
+      finalize(() => console.log('source3$ has completed!'))
+    );
+
+    const source4$ = interval(500).pipe(
+      delay(1000),
+      map((value) => {
+        if (value === 2) throw new Error('Error in observable 4!');
+        return value;
+      }),
+      take(3),
+      finalize(() => console.log('source4$ has completed!'))
+    );
+
+    // source1$.subscribe((val) => console.log('1-> ', val));
+    // source2$.subscribe((val) => console.log('2-> ', val));
+    // source3$.subscribe((val) => console.log('3-> ', val));
+    // source4$.subscribe((val) => console.log('4-> ', val));
+
+    combineLatest([source1$, source2$, source3$, source4$]).subscribe({
+      next: (value) => console.log('Combined -> ', value.join(' - ')),
+      error: (error => console.log('combineLatest ERROR-> ',error)),
+      complete: () => console.log('combineLatest has completed!'),
+    });
   }
 }
