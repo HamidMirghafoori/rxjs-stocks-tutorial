86121a2cf60c939d17b02db36533042ded051171
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-07 19:53:17 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReplaySubject } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -11,6 +12,7 @@ import { TerminalComponent } from '../terminal/terminal.component';
   styleUrl: './intermediate-level.component.css',
 })
 export class IntermediateLevelComponent implements OnInit {
+  private multiCaster$: ReplaySubject<number> = new ReplaySubject<number>();
 
   public constructor(private log: LogService) {}
 
@@ -33,6 +35,28 @@ export class IntermediateLevelComponent implements OnInit {
      * Also see how the moment new subscription happens, it receives all the previous emissions
      */
 
+    this.multiCaster$.subscribe({
+      next: (value) => console.log('Subscriber 1 received value ->', value),
+      error: (error) => console.log('Error happened for 1st subscriber: ', error),
+      complete: () => console.log('Task 1 is completed!'),
+    });
+    this.multiCaster$.next(5);
+    console.log('Now subscriber 2 subscribes...')
+    this.multiCaster$.subscribe({
+      next: (value) => console.log('Subscriber 2 received value ->', value),
+      error: (error) => console.log('Error happened for 2nd subscriber: ', error),
+      complete: () => console.log('Task 2 is completed!'),
+    });
+    this.multiCaster$.next(22);
+    console.log('Now subscriber 3 subscribes...')
+    this.multiCaster$.subscribe({
+      next: (value) => console.log('Subscriber 3 received value ->', value),
+      error: (error) => console.log('Error happened for 3rd subscriber: ', error),
+      complete: () => console.log('Task 3 is completed!'),
+    });
+    this.multiCaster$.next(7);
+    this.multiCaster$.complete();
+    this.multiCaster$.next(100);
   }
 
 }
