c5e91f78a1862adce49d43b649580b897bb281fc
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-08 21:05:08 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { delay, finalize, from, Observable } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -11,9 +12,48 @@ import { TerminalComponent } from '../terminal/terminal.component';
   styleUrl: './intermediate-level.component.css',
 })
 export class IntermediateLevelComponent implements OnInit {
+  private movies: string[] = [
+    'Inception',
+    'The Dark Knight',
+    'Interstellar',
+    'Dunkirk',
+  ];
+  private books: string[] = [
+    'Beloved',
+    'Siddhartha',
+    'Dune',
+    'Inferno',
+    'Frankenstein',
+  ];
+  private songs: string[] = ['Hello', 'Imagine', 'Roar', 'Hurt', 'Yellow'];
+
+  private movies$: Observable<string> = from(this.movies).pipe(
+    delay(3500),
+    finalize(() => console.log('Movies completed'))
+  );
+  private books$: Observable<string> = from(this.books).pipe(
+    delay(2000),
+    finalize(() => console.log('Books completed'))
+  );
+  private songs$: Observable<string> = from(this.songs).pipe(
+    delay(1000),
+    finalize(() => console.log('Songs completed'))
+  );
+  
   public constructor(private log: LogService) {}
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.combineLatestWith();
+  }
+
+  private combineLatestWith() {
+    /**
+     * we modify the final code of combineLatestAll to implement combineLatestWith
+     * use songs$ observable as source and other two (books$ and movies$) as parameters of combineLatestWith
+     * you'll see that the output is exactly the same with combineLatestAll
+     * Note the only difference is the type of source, in combineLatestAll we have observable of observables of(....)
+     */
+
   }
 }
