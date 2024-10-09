5cdd7bd80cdccb618b0e3a5c0025a42df1313aad
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-09 18:03:45 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,19 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.bufferCount();
+  }
+
+  private bufferCount() {
+    /**
+     * To demonstrate bufferCount() functionality we will create 3 observables as following:
+     * interval of 200 which will pipe into bufferCount to buffer 3 emits and takes 4 buffers 
+     * and finally logs the observable has been completed
+     * the 2nd observable will have same structure only buffering 5 and startBufferEvery of 3
+     * the 3rd observable will have same structure only buffering 3 and startBufferEvery of 5
+     * then we want to executed each observable after the previous one is completed
+     * Note how parameters will affect on the output (overlap and gap)
+     */
+
   }
 }
