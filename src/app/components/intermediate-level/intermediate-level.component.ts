90f44552ca004fca9ad080d7eb80ede176447ff3
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-09 19:22:17 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { bufferTime, concat, finalize, interval, take, tap } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -27,5 +28,25 @@ export class IntermediateLevelComponent implements OnInit {
      * the 3rd observable will have same structure only buffering 600 and interval of 1200 and maxBufferSize 3
      * Note how buffer window causes some emits discarded and how maxBufferSize limits the emission length
      */
+    const buffer1$ = interval(200).pipe(
+      bufferTime(800),
+      take(4),
+      finalize(() => console.log('Buffer1$ completed!'))
+    );
+    const buffer2$ = interval(200).pipe(
+      bufferTime(600, 1200),
+      take(4),
+      finalize(() => console.log('Buffer2$ completed!'))
+    );
+    const buffer3$ = interval(200).pipe(
+      bufferTime(600, 1200, 3),
+      take(4),
+      finalize(() => console.log('Buffer3$ completed!'))
+    );
+    concat(
+      buffer1$.pipe(tap((values) => console.log('Buffer1-> ', values))),
+      buffer2$.pipe(tap((values) => console.log('Buffer2-> ', values))),
+      buffer3$.pipe(tap((values) => console.log('Buffer3-> ', values)))
+    ).subscribe();
   }
 }
