b58d2ef16785dcf44c85e79ee5a0ec5469eabbed
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-09 16:11:37 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { concatMap, delay, interval, map, of } from 'rxjs';
+import { buffer, concatMap, delay, interval, map, of, take } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -14,7 +14,7 @@ import { TerminalComponent } from '../terminal/terminal.component';
 export class IntermediateLevelComponent implements OnInit {
   public constructor(private log: LogService) {}
   private getRandomDelay = () => Math.floor(Math.random() * 1500) + 500;
-  // Will emit randomly in range of 500ms to 2000ms 
+  // Will emit randomly in range of 500ms to 2000ms
   private randomInterval$ = interval(0).pipe(
     concatMap((value) => {
       const delayTime = this.getRandomDelay();
@@ -34,13 +34,18 @@ export class IntermediateLevelComponent implements OnInit {
     /**
      * The starting code is provided
      * We will create a source$ observable to emit values every 200ms
-     * To buffer values in a random manner, we will define a bufferNotifier$ observable using randomInterval$, 
+     * To buffer values in a random manner, we will define a bufferNotifier$ observable using randomInterval$,
      * which will randomly emit a signal to release the buffered values up to that point in time.
      * The bufferNotifier$ will emit 10 signals
-     * Finally, we will subscribe to the source$ observable, apply the buffer() operator, 
+     * Finally, we will subscribe to the source$ observable, apply the buffer() operator,
      * and log the values returned by the buffer.
      * Note how buffer collects the values and continues after each closingNotifier
      */
 
+    const source$ = interval(200);
+    const bufferNotifier$ = this.randomInterval$.pipe(take(10));
+    source$
+      .pipe(buffer(bufferNotifier$))
+      .subscribe((data) => console.log(data));
   }
 }
