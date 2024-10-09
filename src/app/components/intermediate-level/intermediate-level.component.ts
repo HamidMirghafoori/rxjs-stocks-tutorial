90367807e7e1030af58d7394587a38a6878effdc
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-09 21:45:58 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,20 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.buffer();
+  }
+
+  private buffer() {
+    /**
+     * Create a source$ observable to emit values every 100ms
+     * To buffer values, define a bufferNotifier$ observable which emits every 1000ms,
+     * The bufferNotifier$ will emit 10 signals
+     * Finally, we will subscribe to the source$ observable to take 55 signals, 
+     * we can tap and log the values of our stream
+     * Apply the buffer() operator which controlled by bufferNotifier$,
+     * and log the values returned by the buffer in subscribe.
+     * Note how buffer collects the values and continues after each closingNotifier.
+     */
+
   }
 }
