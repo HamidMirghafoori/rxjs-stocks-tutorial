8f671fa2e4f2e15b9bf1734956a93338ed6a0575
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-09 19:21:57 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,17 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.bufferTime();
+  }
+
+  private bufferTime() {
+    /**
+     * To demonstrate bufferTime() functionality we will create 3 observables as following:
+     * interval of 200 which will pipe into bufferTime to buffer 800ms emits and takes 4 buffers
+     * and finally logs the observable has been completed
+     * the 2nd observable will have same structure only buffering 600 and interval of 1200
+     * the 3rd observable will have same structure only buffering 600 and interval of 1200 and maxBufferSize 3
+     * Note how buffer window causes some emits discarded and how maxBufferSize limits the emission length
+     */
   }
 }
