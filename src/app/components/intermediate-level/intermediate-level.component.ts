caed40b4ba4636116ff29b26e3b2efd2a67895b0
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-09 16:10:57 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { concatMap, delay, interval, map, of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -12,8 +13,34 @@ import { TerminalComponent } from '../terminal/terminal.component';
 })
 export class IntermediateLevelComponent implements OnInit {
   public constructor(private log: LogService) {}
+  private getRandomDelay = () => Math.floor(Math.random() * 1500) + 500;
+  // Will emit randomly in range of 500ms to 2000ms 
+  private randomInterval$ = interval(0).pipe(
+    concatMap((value) => {
+      const delayTime = this.getRandomDelay();
+      return of(value).pipe(
+        delay(delayTime),
+        map(() => [value, delayTime])
+      );
+    })
+  );
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.buffer();
+  }
+
+  private buffer() {
+    /**
+     * The starting code is provided
+     * We will create a source$ observable to emit values every 200ms
+     * To buffer values in a random manner, we will define a bufferNotifier$ observable using randomInterval$, 
+     * which will randomly emit a signal to release the buffered values up to that point in time.
+     * The bufferNotifier$ will emit 10 signals
+     * Finally, we will subscribe to the source$ observable, apply the buffer() operator, 
+     * and log the values returned by the buffer.
+     * Note how buffer collects the values and continues after each closingNotifier
+     */
+
   }
 }
