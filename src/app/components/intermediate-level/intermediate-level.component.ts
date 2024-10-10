61626b766a398d667743d1ef84f78ca251dd7e9e
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-10 14:24:01 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { interval, take } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -23,6 +24,11 @@ export class IntermediateLevelComponent implements OnInit {
      * Use interval of 100 as source and take only 11 values.
      * Subscribe with complete function signature and log the values and complete message
      */
-
+    interval(100)
+      .pipe(take(11))
+      .subscribe({
+        next: (data) => console.log(data),
+        complete: () => console.log('Interval completed!'),
+      });
   }
 }
