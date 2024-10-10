e502408000f06bc699fe8db46448087ca957fd26
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-10 10:24:11 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,23 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.window();
+  }
+
+  private window() {
+    /**
+     * Create a source$ observable to emit values every 100ms
+     * To buffer values, define a bufferNotifier$ observable which emits every 1000ms,
+     * The bufferNotifier$ will emit 10 signals
+     * Finally, we will subscribe to the source$ observable to take 55 signals,
+     * we can tap and log the values of our stream
+     * Apply the window() operator which controlled by bufferNotifier$,
+     * To convert each observable into array of values use mergeMap() in combination of toArray()
+     * Subscribe to source$ and log the buffered data. 
+     * Note how buffer collects the values and continues after each closingNotifier.
+     * Also note that here we used mergeMap to get access to values 
+     * but in your pipeline you may perform other changes to each window if needed
+     */
+
   }
 }
