70359f964677b967f20a26e07b5034fa78f8b566
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-10 11:39:41 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { concat, finalize, interval, mergeMap, take, tap, toArray, windowCount } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -27,5 +28,30 @@ export class IntermediateLevelComponent implements OnInit {
      * Note how parameters will affect on the output (overlap and gap)
      */
 
+    const buffer1$ = interval(200).pipe(
+      windowCount(3),
+      mergeMap(window$ => window$.pipe(toArray())),
+      take(4),
+      finalize(() => console.log('Buffer1$ completed!'))
+    );
+
+    const buffer2$ = interval(200).pipe(
+      windowCount(5, 3),
+      mergeMap(window$ => window$.pipe(toArray())),
+      take(4),
+      finalize(() => console.log('Buffer2$ completed!'))
+    );
+    const buffer3$ = interval(200).pipe(
+      windowCount(3, 5),
+      mergeMap(window$ => window$.pipe(toArray())),
+      take(4),
+      finalize(() => console.log('Buffer3$ completed!'))
+    );
+
+    concat(
+      buffer1$.pipe(tap((values) => console.log('Buffer1-> ', values))),
+      buffer2$.pipe(tap((values) => console.log('Buffer2-> ', values))),
+      buffer3$.pipe(tap((values) => console.log('Buffer2-> ', values)))
+    ).subscribe();
   }
 }
