d3149698c0186533c4252ed5df36059d28843988
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-10 11:52:42 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,17 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.windowTime();
+  }
+
+  private windowTime() {
+    /**
+     * To demonstrate windowTime() we will modify bufferTime() code as following:
+     * after replacing bufferTime with windowTime we need to convert the window$ observables into
+     * array of values so we be able to log them.
+     * we will implement concat to executed each observable synchronously
+     * Note how buffer window causes some emits discarded and how maxWindowSize limits the emission length
+     */
+
   }
 }
