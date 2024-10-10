78f28e0ab4c31ba5bc8d3b4bad449ea46b0871ed
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-10 15:44:43 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,19 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.takeWhile();
+  }
+
+  private takeWhile() {
+    /**
+     * To compare both arguments of takeWhile follow the steps:
+     * 1 - create counter1$ which is interval of 300 which takeWhile is used to take values less than 4
+     * 2 - To log taken values log them after takeWhile
+     * 3 - At the end log a message that counter1$ completed.
+     * Do the same for counter2$ and for 2nd argument of takeWhile pass true.
+     * Execute two counters synchronously.  and at the end log a message that whole stream completed.
+     * Note the difference between two counters
+     */
+
   }
 }
