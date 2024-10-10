2c527f35197e6250718de422a62085d6c3816d30
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-10 15:05:55 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,16 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.takeUntil();
+  }
+
+  private takeUntil() {
+    /**
+     * We want to take values until we click on document
+     * Create a notifier$ which returns click event from document
+     * Create an interval of 400 and apply takeUntil with notifier$ as argument
+     * Subscribe with complete function signature to log Taken data and completion of observable.
+     */
+
   }
 }
