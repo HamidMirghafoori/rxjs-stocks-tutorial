86a33425fe7805ae28ca601aedd73c6fd9a1f058
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-10 08:53:39 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,15 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.bufferWhen();
+  }
+
+  private bufferWhen() {
+    /**
+     * To explore bufferWhen we create a source$ observable which is interval at 300ms rate
+     * then to see the emitted values we log them and we limit it to take 20 values
+     * we use bufferWhen and pass a function that returns random intervals between 500 to 3000
+     * Subscribe to the source$ and log the buffered data and observe how bufferWhen works
+     */
   }
 }
