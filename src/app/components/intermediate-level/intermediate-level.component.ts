454c0092bc03c4aa15bdd189d565491dcdb2a6ed
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-11 21:29:16 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,18 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.zip();
+  }
+
+  private zip() {
+    const obs1$ = of(1, 2, 3, 4, 5);
+    const obs2$ = of('A', 'B', 'C', 'D');
+    const obs3$ = of(true, false, true);
+    /**
+     * The starting data is provided:
+     * 1 - Pipe zipWith() to obs1$ and pass other two observables to it, subscribe to it and log the values
+     * Note how zip completes when one of the observables completes.
+     */
+
   }
 }
