4ad24719b3ba2f57062b744cf6fb337f8bd5da8e
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-11 16:13:08 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { interval, of, take } from 'rxjs';
+import { groupBy, interval, mergeMap, of, take, toArray } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -43,6 +43,37 @@ export class IntermediateLevelComponent implements OnInit {
      * Note how duration observable limits the number of each
      * group and makes groupBy generate extra emissions for each group.
      */
-    
+    students$
+      .pipe(
+        groupBy((student) => student.grade),
+        mergeMap((group) => group.pipe(toArray()))
+      )
+      .subscribe({
+        next: (data) => console.log(JSON.stringify(data)),
+        complete: () =>
+          console.log('1st group with key selector completed!\n\n'),
+      });
+
+    students$
+      .pipe(
+        groupBy((student) => student.grade, {
+          element: (student) => student.name,
+        }),
+        mergeMap((group) => group.pipe(toArray()))
+      )
+      .subscribe({
+        next: (data) => console.log(JSON.stringify(data)),
+        complete: () =>
+          console.log('2nd group with key and element selector completed!\n\n'),
+      });
+
+    source$
+      .pipe(
+        groupBy((num) => (num % 2 === 0 ? 'even' : 'odd'), {
+          duration: () => interval(1500),
+        }),
+        mergeMap((group) => group.pipe(toArray()))
+      )
+      .subscribe(console.log);
   }
 }
