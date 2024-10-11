b0773d5b9f0c782f01a35f13ad0b2d837cc3bf10
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-11 19:27:02 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,20 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.zip();
+  }
+
+  private zip() {
+    const obs1$ = of(1, 2, 3, 4, 5);
+    const obs2$ = of('A', 'B', 'C', 'D');
+    const obs3$ = of(true, false, true);
+    /**
+     * The starting data is provided:
+     * 1 - Use zip to combine all three observables, subscribe to it and log the values
+     * Note how zip completes when one of the observables completes.
+     * 2 - Create new zip and provide a function to transform the data
+     * use the zip values as arguments and return a string that values are formatted with a dash in between them
+     */
+
   }
 }
