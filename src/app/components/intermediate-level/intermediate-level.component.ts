6904726e21a5467eb56e3ebfafbec69f5765c2c5
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-12 19:20:34 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,16 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.partition();
+  }
+
+  private partition() {
+    /**
+     * Create observable that emits values of 1 to 6 named source$
+     * Use partition which uses source$ and a predicate function that checks if the value is odd or even
+     * Store the return value of partition as evens$ and odds$ observables.
+     * Subscribe to them and log the values.
+     */
+
   }
 }
