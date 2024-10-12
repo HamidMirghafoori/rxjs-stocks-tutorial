8b2035eb3e16a3f1349f488f0cdeea3ab9af3439
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-12 15:40:39 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,21 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.mergeScan();
+  }
+
+  private mergeScan() {
+    /**
+     * 1 - create a function and save it as fakeApiRequest it accepts two arguments
+     * accumulatedTotal and id and returns observable from summing accumulatedTotal with
+     * 100 if id===1, 250 if id===2, and 80 if id===3, default returns observable of accumulatedTotal itself
+     * each return will have 1000ms delay
+     * 2 - create ids$ observable from array of 1 to 3
+     * 3 - apply mergeScan on ids$ by passing accumulator function with default 0 and concurrent = 1
+     * Subscribe to ids$ and log the values
+     * Note how it accumulates the values
+     * Remove concurrent argument to see what happens?
+     */
+  
   }
 }
