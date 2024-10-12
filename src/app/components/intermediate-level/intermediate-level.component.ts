9ac2ae03ee0cf960cbc3f2ad11d6625ad0e2a158
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-12 16:26:51 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { delay, interval, of, switchScan, take } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -30,6 +31,24 @@ export class IntermediateLevelComponent implements OnInit {
      * Subscribe to ids$ and log the values
      * Note how it accumulates the values and when fakeApiRequest takes too long it will discarded
      */
-  
+    const fakeApiRequest = (accumulatedTotal: number, id: number) => {
+      console.log('processing id=', id)
+      switch (id) {
+        case 1:
+          return of(accumulatedTotal + 100).pipe(delay(100));
+        case 2:
+          return of(accumulatedTotal + 250).pipe(delay(600));
+        case 3:
+          return of(accumulatedTotal + 80).pipe(delay(100));
+        default:
+          return of(accumulatedTotal + 10).pipe(delay(10));
+      }
+    };
+
+    const ids$ = interval(500).pipe(take(8));
+
+    ids$
+      .pipe(switchScan((acc, value) => fakeApiRequest(acc, value), 0))
+      .subscribe((result) => console.log(result));
   }
 }
