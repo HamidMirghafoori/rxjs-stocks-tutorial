6a0935e7f0c8fa7e477d1d936fb00bfa3697af73
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-12 22:06:35 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,21 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.throttle();
+  }
+
+  private throttle() {
+    /**
+     * 1 - Define source$ as interval of 500
+     * Create obs1$ with applying throttle to source$ with passing a function that returns interval of 2000
+     * 2 - Copy obs1$ and rename it to obs2$ and apply config in throttle with leading=true and trailing=true
+     * To have separate execution use concat to execute observables synchronously.
+     * subscribe to it and log the values
+     * Note the differences.
+     * Note that for obs2$ we have only one leading value and the rest does not have leading values 
+     * and instead it just emits trailing values. That's because our observable does not emit another value
+     * immediately after training value, so throttle treats the next late coming value as trailing values.
+     */
+
   }
 }
