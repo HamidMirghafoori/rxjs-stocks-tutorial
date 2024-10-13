e75933780b490e655958d5ce689185f64016af38
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-13 18:10:38 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,16 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.count();
+  }
+
+  private count() {
+    const source$ = of(1, 2, 3, 4, 5, 11, 33, 55, 77, 100);
+    /**
+     * source$ is provided,
+     * Apply count to count number of elements
+     * Apply count with predicate function to count odd values
+     */
+
   }
 }
