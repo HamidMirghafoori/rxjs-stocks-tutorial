71e051b0620782b4c4b046fb0d1adcac87e61ece
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-13 13:49:14 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,18 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.debounce();
+  }
+
+  private debounce() {
+    /**
+     * Define clicks$ as document click events and log the time click happened and position of click
+     * then apply debounce() and provide interval of 3000ms, we can take 8 emissions from debounce
+     * Subscribe to clicks$ and log the current time again and using received event log position of click
+     * Note how click events ara logged and how debounce waits for 3 seconds of no emission then
+     * emits the last click event. We used click positions to make it easier to understand which
+     * events are pass through debounce and which ones discarded
+     */
+
   }
 }
