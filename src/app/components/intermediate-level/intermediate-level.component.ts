8bc14bffc262be85f6d562ad44c7e480e0583755
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-13 15:08:18 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,17 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.sample();
+  }
+
+  private sample() {
+    /**
+     * Create source$ which is interval of 1000 and log the value of source
+     * Create notifier$ which is interval of 2500
+     * Apply sample() on source$ with providing notifier$ and take 5 values,
+     * Subscribe and log the sampled values.
+     * Observe how sample() works.
+     */
+
   }
 }
