cf0501a2bbeaa5ee2556f6ac4f32b45d637c3fc5
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 11:17:17 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,17 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.max();
+  }
+
+  private max() {
+    const ranks$ = of(5, 3, 9, 7);
+    const ages$ = of({ age: 25 }, { age: 30 }, { age: 28 });
+    /**
+     * 1 - Apply max on ranks$, subscribe and log the result
+     * 2 - Apply max on ages$ and provide comparer function to compare based on age key
+     *    subscribe and log the result
+     */
+
   }
 }
