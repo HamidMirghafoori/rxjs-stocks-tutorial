837b8dd8b6b8448eb2ce304b1d5ab5857f843dd6
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 20:42:05 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { timer } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -23,6 +24,10 @@ export class IntermediateLevelComponent implements OnInit {
      * 1 - Create source1$ using timer with value 2000. Subscribe and log the values
      * 2 - Create source2$ using timer with value 4000 and interval of 1000. Subscribe and log the values
      */
+    const source1$ = timer(2000);
+    source1$.subscribe((val) => console.log('Source 1:', val));
 
+    const source2$ = timer(4000, 1000);
+    source2$.subscribe((val) => console.log('Source 2:', val));
   }
 }
