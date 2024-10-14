e2c5cb16c48f6451109be48bb11e95a04bfb5cca
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 20:02:59 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,18 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.raceWith();
+  }
+
+  private raceWith() {
+    /**
+     * 1 - Create obs1$ with interval of 7000, map it's value to get message 'slow one #' which # is the value
+     * 2 - Create obs2$ with interval of 3000, map it's value to get message 'fast one #' which # is the value
+     * 3 - Create obs3$ with interval of 5000, map it's value to get message 'medium one #' which # is the value
+     * all three will take 5 values.
+     * Apply raceWith to obs1$ and pass obs2$ and obs3$ as arguments. Subscribe and log the result
+     * Note how only the fastest observable will be emitted til it completes.
+     */
+
   }
 }
