a7804a7f7a6618a6a80ea15466b875fde7818a01
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 11:11:06 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,16 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.findIndex();
+  }
+
+  private findIndex() {
+    const numbers$ = of(10, 20, 30, 40, 50, 60, 70);
+
+    /**
+     * 1- find the first value bigger than 40 and log the result
+     * 2 - find the fist value bigger than 90 and log the result
+     */
+
   }
 }
