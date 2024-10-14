8b615e41b7afe4ee0c4dedd1cda2ca459d97a016
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 21:01:40 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { fromEvent, timeInterval } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -23,6 +24,11 @@ export class IntermediateLevelComponent implements OnInit {
      * Create click$ from click event on document.
      * Apply timeInterval on click$ and subscribe and log a message displaying the time passed since last click.
      */
-
+    const click$ = fromEvent(document, 'click');
+    click$
+      .pipe(timeInterval())
+      .subscribe((data) =>
+        console.log(`Time since last click: ${data.interval}ms`)
+      );
   }
 }
