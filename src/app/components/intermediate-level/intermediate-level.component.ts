d3ba46c7796ed2b6f44aa8e61ef99ad8d706db71
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 09:55:53 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { elementAt, of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -25,6 +26,16 @@ export class IntermediateLevelComponent implements OnInit {
      * 2 - Emit values from 1 to 5, apply elementAt with index 8, provide default value -1
      * Subscribe with complete function signature and log the values.
      */
+    of(1, 2, 3, 4, 5)
+      .pipe(elementAt(2))
+      .subscribe({
+        next: (value) => console.log(value),
+        error: (err) => console.error(err),
+        complete: () => console.log('Complete'),
+      });
 
+    of(1, 2, 3, 4, 5)
+      .pipe(elementAt(8, -1))
+      .subscribe((value) => console.log(value));
   }
 }
