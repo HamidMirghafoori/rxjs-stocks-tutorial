bfc790b71f04caca7080c93e9a45423b101461ee
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 16:33:38 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,16 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.every();
+  }
+
+  private every() {
+    const source1$ = of(2, 4, 6, 8);
+    const source2$ = of(2, 4, 5, 8);
+    /**
+     * 1 - Apply every to source1$ check if the value is even, subscribe and log the result
+     * 2 - Apply every to source2$ check if the value is even, subscribe and log the result
+     */
+    
   }
 }
