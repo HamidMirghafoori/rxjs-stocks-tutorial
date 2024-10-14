e065343db349190639001c58a8c54bd92501aacb
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-14 09:55:41 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,16 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.elementAt();
+  }
+
+  private elementAt() {
+    /**
+     * 1 - Emit values from 1 to 5, apply elementAt with index 2, subscribe with complete function signature
+     *    and log the value
+     * 2 - Emit values from 1 to 5, apply elementAt with index 8, provide default value -1
+     * Subscribe with complete function signature and log the values.
+     */
+
   }
 }
