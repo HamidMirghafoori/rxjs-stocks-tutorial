3f8044797ae958e11273959bbd702e26df983bc8
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-15 21:11:43 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,21 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.from();
+  }
+
+  private from() {
+    const numbers = [10, 20, 30];
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve('Promise resolved!'), 1000);
+    });
+
+    /**
+     * 1 - Create observable1$ from numbers, subscribe and log the values
+     * 2 - Create observable2$ from promise, subscribe with complete function signature and log values.
+     * 3 - Create observable3$ from string 'Hello', subscribe and log the values
+     * Observe how from converts different data types and how from emits them.
+     */
+
   }
 }
