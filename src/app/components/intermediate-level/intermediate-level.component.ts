c1b61a1369c0a0a1b3fc79d8e0a391f19aaa58fc
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-15 12:39:46 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,17 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.pipe();
+  }
+
+  private pipe() {
+    const numbers$ = of(1, 2, 3, 4, 5);
+    /**
+     * use pipe on numbers$ provide a function which catches the value emitted from numbers$ and
+     * subscribe to the value and log the values with a message to identify message comes from which log
+     * then subscribe to numbers$ too and log its values too (with a message to identify the log)
+     * Note how pipe emits the original observable, so we have access to it to perform more transformation on data
+     */
+
   }
 }
