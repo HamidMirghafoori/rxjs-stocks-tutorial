aadd9d63baf3bca3ef51c809b12c11d3a39f33ca
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-15 20:01:38 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,15 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.isObservable();
+  }
+
+  private isObservable() {
+    const obs$ = of(1, 2, 3);
+    const notObs = [1, 2, 3];
+    /**
+     * Use isObservable to check if obs$ and notObs are observables or not
+     */
+
   }
 }
