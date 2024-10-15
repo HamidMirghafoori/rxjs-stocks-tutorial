bb4e025725567637858e4f6b37bbc8244217dd74
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-15 12:59:09 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { map, of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -23,6 +24,8 @@ export class IntermediateLevelComponent implements OnInit {
      * Emit values from 1 to 4 and apply map to multiply them by 2.
      * Subscribe and log the result
      */
-
+    of(1, 2, 3, 4)
+      .pipe(map((value) => value * 2))
+      .subscribe((result) => console.log(result));
   }
 }
