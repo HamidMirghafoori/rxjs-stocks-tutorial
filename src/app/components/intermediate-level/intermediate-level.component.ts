55f5b8041fc6a515adbf13afe4de8422beec1606
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-15 16:04:06 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {
+  fromEvent,
+  interval,
+  map,
+  merge,
+  NEVER,
+  startWith,
+  switchMap,
+} from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -29,6 +38,20 @@ export class IntermediateLevelComponent implements OnInit {
      * switch to timer$ or NEVER with any action received
      * Subscribe and click on start and pause buttons to log the values
      */
+    const startClick$ = fromEvent(startButton, 'click').pipe(
+      map(() => 'start')
+    );
+    const pauseClick$ = fromEvent(pauseButton, 'click').pipe(
+      map(() => 'pause')
+    );
 
+    const timer$ = interval(1000);
+
+    const timerControl$ = merge(startClick$, pauseClick$).pipe(
+      startWith('pause'),
+      switchMap((action) => (action === 'start' ? timer$ : NEVER))
+    );
+
+    timerControl$.subscribe((value) => console.log(`Timer: ${value}`));
   }
 }
