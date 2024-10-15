b35d6ca4f043eb25b8c0f8464c18b857652f48c2
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-15 14:20:30 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -15,5 +15,15 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.repeat();
+  }
+
+  private repeat() {
+    /**
+     * 1 - create source1$ with interval of 100, take 3 values and repeat it 2 times. subscribe and log the values
+     * 2 - create source2$ with interval of 1000, take 3 and repeat it with config object count 3 and delay 1000,
+     * subscribe and log the values note how config applied to the repeat.
+     */
+
   }
 }
