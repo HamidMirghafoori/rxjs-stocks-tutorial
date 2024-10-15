2f687aa48569a88aa312cfd482cb59b204e3a799
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-15 16:54:14 +0100

    answer

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { EMPTY, interval, mergeMap, of, take } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -27,5 +28,14 @@ export class IntermediateLevelComponent implements OnInit {
      * Observe how EMPTY causes the values get ignored
      */
 
+    const interval$ = interval(100);
+    const result = interval$.pipe(
+      mergeMap((x) => (x % 2 === 1 ? of(x) : EMPTY)),
+      take(10)
+    );
+    result.subscribe({
+      next: (x) => console.log(x),
+      complete: () => console.log('Completed!'),
+    });
   }
 }
