2f6786625795e8dc24e38cb6afcd5a44d74cb709
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-18 20:26:56 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,22 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.distinctUntilKeyChanged();
+  }
+
+  private distinctUntilKeyChanged() {
+    const source$ = of(
+      { id: 1, name: 'A' },
+      { id: 1, name: 'B' },
+      { id: 2, name: 'A' },
+      { id: 1, name: 'C' },
+      { id: 3, name: 'C' },
+      { id: 3, name: 'C' }
+    );
+    /**
+     * Apply distinctUntilKeyChanged on source$ and pass 'id' as parameter to filter based on id
+     * subscribe and log the values. You can try 'name' to observe the difference
+     */
+
   }
 }
