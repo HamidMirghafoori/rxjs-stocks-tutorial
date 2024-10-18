82d30655d28771cd4beb5f235969fbbbe6e1276d
Hamid Mirghafoori <mirghafooris@ryanair.com>
2024-10-18 19:22:58 +0100

    question

--- src/app/components/intermediate-level/intermediate-level.component.ts
+++ src/app/components/intermediate-level/intermediate-level.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { LogService } from '../../services';
 import { TerminalComponent } from '../terminal/terminal.component';
 
@@ -15,5 +16,26 @@ export class IntermediateLevelComponent implements OnInit {
 
   public ngOnInit(): void {
     this.log.clearLogs();
+    this.distinctUntilChanged();
+  }
+
+  private distinctUntilChanged() {
+    const source1$ = of(1, 2, 2, 3, 3, 3, 4, 1, 2, 2, 1);
+    const source2$ = of(
+      { id: 1, name: 'A' },
+      { id: 2, name: 'B' },
+      { id: 1, name: 'A' },
+      { id: 1, name: 'C' },
+      { id: 3, name: 'C' },
+      { id: 3, name: 'C' }
+    );
+    /**
+     * 1 - Apply distinctUntilChanged on source1$, subscribe and log the values
+     * 2 - Apply distinctUntilChanged on source2$, pass comparator function which
+     * simply checks if both values are equal, and pass keySelector function to
+     * selects id for comparison, subscribe and log respectively
+     * 3 - Same as question 2 but use name key for key selector
+     */
+
   }
 }
